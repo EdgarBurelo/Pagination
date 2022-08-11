5d402f8b9ce7be988de1512837e2755858f6a69b
EdgarGuerrero <edgar.burelo@gmail.com>
2022-08-11 11:49:45 +0200

    add Effect for changing active page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Paginate from "./Pagination/Paginate";
 
 const App: React.FC = () => {
+    const [page, setPage] = useState(1);
+    const setPageGenCallback = (page: number) => {
+        console.log(page)
+    }
+
     return (
         <div>
             Case for less than 10 pages starting on 3
-            <Paginate currentPage={3} totalPages={5} />
+            <Paginate currentPage={3} totalPages={5} setPage={setPageGenCallback} />
             <br />
             Case for more pages starting in 1 page
-            <Paginate currentPage={1} totalPages={12} />
+            <Paginate currentPage={1} totalPages={12} setPage={setPageGenCallback} />
             <br />
             Case for more pages starting in 7 page
-            <Paginate currentPage={7} totalPages={12} />
+            <Paginate currentPage={7} totalPages={12} setPage={setPageGenCallback} />
             <br />
             Case for more pages starting in the middle page
-            <Paginate currentPage={10} totalPages={20} />
+            <Paginate currentPage={10} totalPages={20} setPage={setPageGenCallback} />
             <br />
+            Case of a one with the logic lager
+            <Paginate currentPage={page} totalPages={20} setPage={setPage} />
         </div>
     );
 };
--- src/Pagination/Paginate.tsx
+++ src/Pagination/Paginate.tsx
@@ -1,45 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Paginate.scss";
 import BorderedButton from "../DesignComponents/Buttons/BorderedButton";
 
 type Paginate = {
     currentPage: number
     totalPages: number
+    setPage: (pageNumber: number) => void
 }
 
 type onClickPageButton = (event: React.MouseEvent<HTMLElement> & { target: HTMLElement }) => void
 
-const Paginate: React.FC<Paginate> = ({ currentPage, totalPages }) => {
+const Paginate: React.FC<Paginate> = ({ currentPage, totalPages, setPage }) => {
     if (currentPage > totalPages) throw new Error('The current page can not be bigger that the total pages');
     const maxDisplayedPages = 10;
     const shownPages = totalPages / maxDisplayedPages > 1 ? 10 : totalPages
 
     const onClickPageButton: onClickPageButton = (event) => {
         const pageId: number = parseInt(event.target.id.split("-")[1]);
-        console.log(pageId);
+        setPage(pageId);
         // add Generic callback for the click event over any button
     }
-
-    let start:number
-    let end:number
     const differenceBetweenCurrentAndTotal = totalPages - currentPage;
 
-    if (shownPages < maxDisplayedPages) {
-        end = shownPages;
-        start = 1;
-    } else if(currentPage < shownPages) {
-        end = shownPages;
-        start = 1;
-    } else if (differenceBetweenCurrentAndTotal < shownPages / 2) {
-        end = totalPages
-        start = currentPage - (shownPages - differenceBetweenCurrentAndTotal)
-    } else if (differenceBetweenCurrentAndTotal > shownPages / 2) {
-        end = currentPage + shownPages / 2 - 1;
-        start = currentPage - shownPages / 2;
-    } else {
-        start = 1;
-        end = shownPages;
-    }
+    let [start, end] = calculateStart(shownPages, maxDisplayedPages, currentPage, differenceBetweenCurrentAndTotal, totalPages);
+
+    useEffect(() => {
+        [start, end] = calculateStart(shownPages, maxDisplayedPages, currentPage, differenceBetweenCurrentAndTotal, totalPages);
+        console.log('recalc', start, end)
+    }, [currentPage, totalPages, differenceBetweenCurrentAndTotal, maxDisplayedPages, shownPages])
 
     const buttons = generateButtons(start, end, currentPage, totalPages, onClickPageButton);
 
@@ -55,8 +43,8 @@ const Paginate: React.FC<Paginate> = ({ currentPage, totalPages }) => {
 const generateButtons = (start: number, end: number, current: number, lastPage: number, fn: onClickPageButton) => {
     const buttons = [];
     for (let i = start; i < end + 1; i++) {
-        let label = i === start ? 'First' : `${i}`
-        label = i === end ? 'Last' : label
+        let label = i === start ? `First (1)` : `${i}`
+        label = i === end ? `Last (${lastPage})` : label
 
         let id = i === start ? 1 : i;
         id = i === end ? lastPage : id
@@ -73,6 +61,35 @@ const generateButtons = (start: number, end: number, current: number, lastPage: 
         buttons.push(button);
     }
     return buttons;
+}
+
+const calculateStart = (
+    shownPages: number,
+    maxDisplayedPages: number,
+    currentPage: number,
+    differenceBetweenCurrentAndTotal: number,
+    totalPages: number
+): [number, number] => {
+    let start: number
+    let end: number
+
+    if (shownPages < maxDisplayedPages) {
+        end = shownPages;
+        start = 1;
+    } else if(currentPage < shownPages / 2) {
+        end = shownPages;
+        start = 1;
+    } else if (differenceBetweenCurrentAndTotal < shownPages / 2) {
+        end = totalPages
+        start = currentPage - (shownPages - differenceBetweenCurrentAndTotal)
+    } else if (differenceBetweenCurrentAndTotal > shownPages / 2) {
+        end = currentPage + shownPages / 2 - 1;
+        start = currentPage - shownPages / 2;
+    } else {
+        start = 1;
+        end = shownPages;
+    }
+    return [start, end];
 }
 
 export default Paginate;
